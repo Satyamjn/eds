b25dbcb9cefcdf41ac637efe2e5cf568d1902863
Satyam Jain <149857464+Satyamjn@users.noreply.github.com>
2025-09-01 16:06:33 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,1 @@
-@@ .. @@
- import { defineConfig } from 'vite'
- import react from '@vitejs/plugin-react'
- 
- // https://vitejs.dev/config/
- export default defineConfig({
-   plugins: [react()],
-+  optimizeDeps: {
-+    include: ['three', '@react-three/fiber', '@react-three/drei']
-  },
-  server: {
-    host: true,
-    port: 5173
-  }
-+  }
- })
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
